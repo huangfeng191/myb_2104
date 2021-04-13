1605b773370b54c0841fb795c49f154322e6ed4b
fengfeng <huangfeng191@163.com>
2021-04-13 09:56:19 +0800

    add redis

--- src/test/java/org/fengfeng/service/PeopleTest.java
+++ src/test/java/org/fengfeng/service/PeopleTest.java
@@ -21,12 +21,15 @@ public class PeopleTest {
     @Test
     public void getPeople() throws IOException {
         Integer id =1;
+        Integer id2=2;
         SqlSessionFactory sqlSessionFactory = getSqlSessionFactory();
         SqlSession sqlSession = sqlSessionFactory.openSession();
         try{
             PeopleMapper mapper = sqlSession.getMapper(PeopleMapper.class);
             People people = mapper.selectPeople(id);
+            People people2 = mapper.selectPeople(id2);
             System.out.println(people);
+            System.out.println(people2);
         }finally {
             sqlSession.close();
         }
